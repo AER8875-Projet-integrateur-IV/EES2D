0d5bf7feb652cfa48094af49c6422a8a6179ba02
rouga <rouga2012@hotmail.fr>
2020-10-01 12:17:55 -0400

    use pointer to call Su2MESH

--- src/Euler2D.cpp
+++ src/Euler2D.cpp
@@ -6,7 +6,7 @@ using namespace ees2d::Mesh;
 
 int main() {
 	std::cout << "Euler2D Software" << std::endl;
-	std::string path = "../../tests/mesh/square.su2";
-	Su2Mesh mymesh{path};
-	mymesh.Parse();
+	::std::string path = "../../tests/mesh/naca0012_euler_65x65x1_O_1B.su2";
+	std::unique_ptr<Su2Mesh> mymesh(std::make_unique<Su2Mesh>(path));
+	mymesh->Parse();
 }
