315d8eb198f5c147132b84b71892399d0f7e9258
rouga <rouga2012@hotmail.fr>
2020-10-03 18:26:05 -0400

    fix test bugs

--- tests/mesh/test_dummy.cpp
+++ tests/mesh/test_dummy.cpp
@@ -2,17 +2,21 @@
 #include <gmock/gmock.h>    // Toujours inclu
 
 
-float Division(int a, int b){
-    return a/b;
+double Division(double a, double b){
+    if (b == 0){
+        return 0;
+    }
+    else 
+        return a/b;
 }
 
-TEST(Test_Dummy, Test_Dummy_1) {
+TEST(Test_Dummy, Dummy_1) {
     int a = 2;
     int b = 4;
     double c = Division(a,b);
     EXPECT_EQ(c, 0.5);
 }
 
-TEST(Test_Dummy, Test_Dummy_1) {
+TEST(Test_Dummy, Dummy_2) {
     EXPECT_EQ(Division(1,0), 0); 
 }
