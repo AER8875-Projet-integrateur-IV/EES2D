ad09c41f9d4b7c3631943d1e584d18b3a672f9e0
rouga <rouga2012@hotmail.fr>
2020-10-04 20:35:48 -0400

    rename Mesh to Parser

---
+++ src/io/Su2Parser.cpp
@@ -0,0 +1,100 @@
+/* ---------------------------------------------------------------------
+ *
+ * Copyright (C) 2020 - by the EES2D  authors
+ *
+ * This file is part of EES2D.
+ *
+ *   EES2D is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   EES2D is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with EES2D.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------
+ *
+ * Authors: Amin Ouled-Mohamed & Ali Omais, Polytechnique Montreal, 2020-
+ */
+
+#include <stdlib.h>
+
+#include <iostream>
+#include <sstream>
+
+#include "Mesh.h"
+
+using namespace ees2d::IO;
+using std::cout, std::endl, std::cerr;
+
+Su2Parser::Su2Parser(const std::string &path) : Parser::Parser(path) {
+
+	std::ifstream m_fileIO{m_path};
+	if (m_fileIO.is_open()) {
+		cout << "Su2 Mesh file found\n"
+		     << "SU2 Parser Initialized !" << endl;
+		m_proceed = true;
+	} else {
+		cerr << "Unable to open mesh file !" << endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+Su2Parser::~Su2Parser() { cout << "SU2 Parser Destroyed !" << endl; }
+
+void Su2Parser::parseCOORDS(std::ifstream &m_fileIO) {
+	std::string line;
+	m_fileIO.seekg(m_fileIO.beg);
+	while (std::getline(m_fileIO, line)) {
+		if (line.find("NPOIN") != std::string::npos) {
+			std::stringstream ss(line);//Temporary stringstream to parse int from string
+			ss.seekg(6) >> m_Ngrids;   //Begin at Pos num 6 to extract int : NPOIN = 2
+			cout << "Number of points : " << m_Ngrids << endl;
+			break;
+		}
+	}
+}
+
+void Su2Parser::parseCONNEC(std::ifstream &m_fileIO) {
+	std::string line;
+	m_fileIO.seekg(m_fileIO.beg);
+	while (std::getline(m_fileIO, line)) {
+		if (line.find("NELEM") != std::string::npos) {
+			std::stringstream ss(line);//Temporary stringstream to parse int from string
+			ss.seekg(6) >> m_Nelems;   //Begin at Pos num 6 to extract int : NELEM = 2
+			cout << "Number of elements : " << m_Nelems << endl;
+			break;
+		}
+	}
+}
+
+void Su2Parser::parseNPSUE(std::ifstream &m_fileIO) {}
+
+void Su2Parser::parseDim(std::ifstream &m_fileIO) {
+	std::string line;
+	while (std::getline(m_fileIO, line)) {
+
+		if (line.find("NDIME") != std::string::npos) {
+			std::stringstream ss(line);//Temporary stringstream to parse int from string
+			ss.seekg(6) >> m_Ndim;     //Begin at Pos num 6 to extract int : NDIME= 2
+			cout << "Number of dimensions : " << m_Ndim << endl;
+			break;
+		}
+	}
+}
+
+void Su2Parser::Parse() {
+
+	if (m_proceed) {
+		std::ifstream m_fileIO(m_path);
+		parseDim(m_fileIO);
+		parseCOORDS(m_fileIO);
+		parseCONNEC(m_fileIO);
+		parseNPSUE(m_fileIO);
+	}
+}
