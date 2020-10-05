f24f2c5e8d53c52cfc78d38349c046e6ad942d2b
rouga <rouga2012@hotmail.fr>
2020-10-04 20:03:40 -0400

    rename mesh folder to io and Euler2D.cpp file to EES2D.cpp

---
+++ src/EES2D.cpp
@@ -0,0 +1,39 @@
+/* ---------------------------------------------------------------------
+ *
+ * Copyright (C) 2020 - by the EES2D  authors
+ *
+ * This file is part of EES2D.
+ *
+ *   EES2D is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   EES2D is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with EES2D.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------
+ *
+ * Authors: Amin Ouled-Mohamed & Ali Omais, Polytechnique Montreal, 2020-
+ */
+
+#include "mesh/Mesh.h"
+#include <iostream>
+//include "utils/Timer.h"
+
+using namespace ees2d::IO;
+//using namespace ees2d::Utils;
+
+
+int main() {
+  std::cout << "Euler2D Software" << std::endl;
+  std::string path = "../../tests/mesh/naca0012_euler_65x65x1_O_1B.su2";
+  std::unique_ptr<Su2Mesh> mymesh(std::make_unique<Su2Mesh>(path));
+  //Timer timeit(std::string("Su2Mesh class"));
+  mymesh->Parse();
+}
---
+++ src/io/Mesh.h
@@ -0,0 +1,72 @@
+/* ---------------------------------------------------------------------
+ *
+ * Copyright (C) 2020 - by the EES2D authors
+ *
+ * This file is part of EES2D.
+ *
+ *   EES2D is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   EES2D is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with EES2D.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------
+ *
+ * Authors: Amin Ouled-Mohamed & Ali Omais, Polytechnique Montreal, 2020-
+ */
+
+#pragma once
+
+#include <fstream>
+#include <memory>
+#include <string>
+#include <unordered_map>
+namespace ees2d::IO {
+
+  class AbstractMesh {
+public:
+	explicit AbstractMesh(const std::string &path) : m_path(path){};
+	virtual ~AbstractMesh(){};
+
+	virtual void parseDim(std::ifstream &) = 0;
+	virtual void parseCOORDS(std::ifstream &) = 0;
+	virtual void parseCONNEC(std::ifstream &) = 0;
+	virtual void parseNPSUE(std::ifstream &) = 0;
+	virtual void Parse() = 0;
+
+public:
+	bool m_proceed = false;
+	std::string m_path;
+	int m_Ndim{0};
+	int m_Ngrids{0};
+	int m_Nelems{0};
+	std::unique_ptr<double[]> m_COORDS{nullptr};// Careful : Works only on C++17 compatible compiler
+	std::unique_ptr<int[]> m_CONNEC{nullptr};
+	std::unique_ptr<int[]> m_NPSUE{nullptr};
+  };
+
+  class Su2Mesh : public AbstractMesh {
+public:
+	explicit Su2Mesh(const std::string &path);
+	~Su2Mesh() override;
+	void parseDim(std::ifstream &m_fileIO) override;
+	void parseCOORDS(std::ifstream &) override;
+	void parseCONNEC(std::ifstream &) override;
+	void parseNPSUE(std::ifstream &) override;
+	void Parse() override;
+
+	// Unordered_map to define VTK_cells with following structure -> {vtk_cell_id : number_of_points}
+	std::unordered_map<int, int> Vtk_Cell = {
+	        {3, 2},
+	        {5, 3},
+	        {9, 4},
+	};
+  };
+}// namespace ees2d::IO
