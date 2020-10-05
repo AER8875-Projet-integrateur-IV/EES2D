1337b74a2ad293fdace97551fa09994799a63282
rouga <rouga2012@hotmail.fr>
2020-10-04 20:04:20 -0400

    2 spaces for tab instead of 4

--- src/utils/Timer.cpp
+++ src/utils/Timer.cpp
@@ -2,27 +2,27 @@
 #include <iostream>
 using namespace ees2d::Utils;
 
-Timer::Timer(std::string tag) 
-:m_tag(tag)
-{
+Timer::Timer(std::string tag)
+    : m_tag(tag) {
 
-	m_startTime = std::chrono::high_resolution_clock::now();
+  m_startTime = std::chrono::high_resolution_clock::now();
 }
 
 Timer::~Timer() {
 
-	stop();
+  stop();
 }
 
 
 void Timer::stop() {
 
-	auto endTime = std::chrono::high_resolution_clock::now();
-	auto start = std::chrono::time_point_cast<std::chrono::microseconds>(m_startTime).time_since_epoch().count();
-	auto end = std::chrono::time_point_cast<std::chrono::microseconds>(endTime).time_since_epoch().count();
+  auto endTime = std::chrono::high_resolution_clock::now();
+  auto start = std::chrono::time_point_cast<std::chrono::microseconds>(m_startTime).time_since_epoch().count();
+  auto end = std::chrono::time_point_cast<std::chrono::microseconds>(endTime).time_since_epoch().count();
 
-	auto duration = end - start;
-	double ms = duration * 0.001;
+  auto duration = end - start;
+  double ms = duration * 0.001;
 
-	std::cout <<"[ " << m_tag << " ] "<< "Profiling results : " << duration << "us (" << ms << "ms)" << std::endl;
+  std::cout << "[ " << m_tag << " ] "
+            << "Profiling results : " << duration << "us (" << ms << "ms)" << std::endl;
 }
--- tests/mesh/test_dummy.cpp
+++ tests/mesh/test_dummy.cpp
@@ -1,22 +1,21 @@
-#include <gtest/gtest.h>    // Toujours inclu
-#include <gmock/gmock.h>    // Toujours inclu
+#include <gmock/gmock.h>// Toujours inclu
+#include <gtest/gtest.h>// Toujours inclu
 
 
-double Division(double a, double b){
-    if (b == 0){
-        return 0;
-    }
-    else 
-        return a/b;
+double Division(double a, double b) {
+  if (b == 0) {
+	return 0;
+  } else
+	return a / b;
 }
 
 TEST(Test_Dummy, Dummy_1) {
-    int a = 2;
-    int b = 4;
-    double c = Division(a,b);
-    EXPECT_EQ(c, 0.5);
+  int a = 2;
+  int b = 4;
+  double c = Division(a, b);
+  EXPECT_EQ(c, 0.5);
 }
 
 TEST(Test_Dummy, Dummy_2) {
-    EXPECT_EQ(Division(1,0), 0); 
+  EXPECT_EQ(Division(1, 0), 0);
 }
