454659da3c978fe44d86232b174025e8f14415d9
rouga <rouga2012@hotmail.fr>
2020-10-15 19:31:30 -0400

    new files

---
+++ src/mesh/Metrics.h
@@ -0,0 +1,37 @@
+/*
+* This file is part of EES2D.
+*
+*   EES2D is free software: you can redistribute it and/or modify
+        *   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   EES2D is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with EES2D.  If not, see <https://www.gnu.org/licenses/>.
+*
+* ---------------------------------------------------------------------
+*
+* Authors: Amin Ouled-Mohamed & Ali Omais, Polytechnique Montreal, 2020-
+*/
+
+#pragma once
+#include "mesh/Connectivity.h"
+
+using ees2d::mesh::Connectivity;
+
+
+class Metrics {
+	public:
+	Metrics(Connectivity &);
+
+
+	void computeFaceMetrics();
+	void computeCvMetrics();
+
+	private:
+};
